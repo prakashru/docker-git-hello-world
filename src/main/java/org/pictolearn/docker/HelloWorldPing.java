78ae882b435e75aadb29dfa4550d0efcb0eb1bec
System Administrator <root@Prakashs-MacBook-Air.local>
2020-09-04 18:27:31 +0530

    Update java class

--- src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping --> version 1 " + i );
+			System.out.println("Hello World Ping --> version 1 (This is a test of continuous integration) " + i );
 			Thread.sleep(1000);
 		}
 	}
